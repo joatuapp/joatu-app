44da60a803f26a5471d4196f1b9e6f173d9d5015
Alex Willemsma <alex@undergroundwebdevelopment.com>
2014-07-13 10:24:44 -0700

    Detailed Offer policy and added specs for it.

--- spec/policies/offer_spec.rb
+++ spec/policies/offer_spec.rb
@@ -1,9 +1,9 @@
 require 'policy_spec_helper'
 
-describe UserPolicy do
-  subject { UserPolicy }
+describe OfferPolicy do
+  subject { OfferPolicy }
 
-  let(:record) { FactoryGirl.create(:user) }
+  let(:record) { FactoryGirl.create(:offer_base) }
 
   permissions :index? do
     it_behaves_like "it grants anyone access"
@@ -14,7 +14,7 @@ describe UserPolicy do
   end
   
   permissions :create? do
-    it_behaves_like "it grants anyone access"
+    it_behaves_like "it grants owner and admin access"
   end
 
   permissions :update? do
@@ -25,20 +25,18 @@ describe UserPolicy do
     it_behaves_like "it grants owner and admin access"
   end
 
-  context "policy scoped users" do
+  context "policy scoped offers" do
     let(:count) { rand(5) + 3 }
     let(:current_user) { FactoryGirl.create(:user) }
 
     before :each do
       count.times do
-        FactoryGirl.create(:user)
+        FactoryGirl.create(:offer_base)
       end
     end
     
-    it "returns all users" do
-      # NOTE: Should match count + 1 because we're also creating a user to be
-      # the current_user, and that user should be returned as well.
-      expect(Pundit.policy_scope(current_user, User).all.size).to eq (count + 1)
+    it "returns all offers" do
+      expect(Pundit.policy_scope(current_user, Offer::Base).all.size).to eq count
     end
   end
 end
