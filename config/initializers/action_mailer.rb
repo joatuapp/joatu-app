41da79e65dead214a0975eca2dee1123818b83a0
Alex Willemsma <alex@undergroundwebdevelopment.com>
2014-09-29 12:20:50 -0700

    Update Mandrill credentials to use a dedicated JoatU Mandrill account.

--- config/initializers/action_mailer.rb
+++ config/initializers/action_mailer.rb
@@ -7,15 +7,11 @@ module JoatuApp
     config.action_mailer.smtp_settings = {
       address: 'smtp.mandrillapp.com',
       port: 587,
-      user_name: 'alex@undergroundwebdevelopment.com',
+      user_name: 'info@joatu.com',
       password: ENV.fetch('SMTP_PASSWORD'),
       authentication: :login,
       enable_starttls_auto: true,
       domain: ENV.fetch('APP_HOST'),
-    }
-
-    config.action_mailer.default_options = {
-      'X-MC-Subaccount' => 'joatu',
     }
   end
 end
